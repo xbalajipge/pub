54f860fa28e83a72807e61ce0ee14cdd9f7a76cf
Balaji Venkataraman (xbalajipge) <xbalaji@pge.com>
2021-10-04 15:42:56 -0700

    Create init.ps1

---
+++ init.ps1
@@ -0,0 +1,13 @@
+echo "powershell script to execute"
+echo "curl -skLq <url-to-this-file> | powershell.exe"
+
+$env:AWS_DEFAULT_PROFILE = 'appstream_machine_role'
+
+echo "list directory"
+aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/
+
+echo "list executable"
+aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/ArcGISPro_28_177688.exe
+
+echo "list patch"
+aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/patches/ArcGIS_Pro_281_177644.msp
