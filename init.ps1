2c2d89e1ab8e9587bf5b5892380ef0e82f3b154d
Balaji Venkataraman <b1v6@pge.com>
2021-10-04 15:50:07 -0700

    update copying

--- init.ps1
+++ init.ps1
@@ -1,6 +1,7 @@
 # simple power shell script to be sourced
 echo "powershell script to execute"
 echo "curl -skLq <url-to-this-file> | powershell.exe"
+echo "curl -skLq https://raw.githubusercontent.com/xbalajipge/pub/main/init.ps1 | powershell.exe"
 
 $env:AWS_DEFAULT_PROFILE = 'appstream_machine_role'
 
@@ -12,3 +13,10 @@ aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/ArcGISPro_28_177688.ex
 
 echo "list patch"
 aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/patches/ArcGIS_Pro_281_177644.msp
+
+
+echo "copying from s3 bucket"
+aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/ArcGISPro_28_177688.exe .
+aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/patches/ArcGIS_Pro_281_177644.msp .
+
+ls
