ea0481d31964940daaf1f9bf4269620818f1a028
xbalaji <xbalaji@gmail.com>
2021-10-04 17:44:44 -0700

    added REM statement for batch file comments

--- init.bat
+++ init.bat
@@ -1,4 +1,4 @@
-# simple power shell script to be sourced
+REM simple power shell script to be sourced
 echo "powershell script to execute"
 echo "curl -skLq <url-to-this-file> | powershell.exe"
 echo "curl -skLq https://raw.githubusercontent.com/xbalajipge/pub/main/init.bat | cmd.exe"
@@ -8,25 +8,26 @@ set HOME=C:\Users\ImageBuilderAdmin
 set USR_DNLD=%HOME%\Downloads
 set ARCGIS_DNLD=%HOME%\ArcGIS_Download
 
-# download 7zip
+REM download 7zip
 curl -skLq https://www.7-zip.org/a/7z1900-x64.exe -o %HOME%\7zip.exe
 start /wait %HOME%\7zip.exe /S
 
-#echo "list directory"
-#aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/
-#echo "list executable"
-#aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/ArcGISPro_28_177688.exe
-#echo "list patch"
-#aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/patches/ArcGIS_Pro_281_177644.msp
+REM echo "list directory"
+REM aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/
+REM echo "list executable"
+REM aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/ArcGISPro_28_177688.exe
+REM echo "list patch"
+REM aws s3 ls s3://geomartcloud-installer/esri/arcgis_pro_2_8/patches/ArcGIS_Pro_281_177644.msp
 
 
-# create a directory
-mkdir %ARCGIS_DNLD_DIR%
+REM create a directory
+mkdir %ARCGIS_DNLD%
+
 echo "copying from s3 bucket"
-#aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/ArcGISPro_28_177688.exe .
-#aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/patches/ArcGIS_Pro_281_177644.msp .
+REM aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/ArcGISPro_28_177688.exe .
+REM aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/patches/ArcGIS_Pro_281_177644.msp .
 
-aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/ArcGISPro_28_177688.exe %ARCGIS_DNLD_DIR%
-aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/patches/ArcGIS_Pro_281_177644.msp %ARCGIS_DNLD_DIR%
+aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/ArcGISPro_28_177688.exe %ARCGIS_DNLD%
+aws s3 cp s3://geomartcloud-installer/esri/arcgis_pro_2_8/patches/ArcGIS_Pro_281_177644.msp %ARCGIS_DNLD%
 
-ls %ARCGIS_DNLD_DIR%
+dir %ARCGIS_DNLD%
